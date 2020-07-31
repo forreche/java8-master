c764feba9ae998a893fefbae27f3b3da80cdefd5
forreche <516108074@qq.com>
2020-07-31 15:35:18 +0800

    首次提交

---
+++ java8-example/src/main/java/com/fangxiaobin/java8/chatper1/Apple.java
@@ -0,0 +1,11 @@
+package com.fangxiaobin.java8.chatper1;
+
+/**
+ * @author :方晓彬
+ * @Date :Created in 15:28 2020/7/31
+ * description:
+ */
+public class Apple {
+
+
+}
---
+++ java8-example/src/main/java/com/fangxiaobin/java8/chatper1/FilterApple.java
@@ -0,0 +1,25 @@
+package com.fangxiaobin.java8.chatper1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author :方晓彬
+ * @Date :Created in 15:29 2020/7/31
+ * description:
+ */
+public class FilterApple {
+
+    public static List<Apple> findGreenApple(List<Apple> apples){
+        List<Apple> list = new ArrayList<>();
+        for (Apple apple : apples) {
+            list.add(apple);
+        }
+        return list;
+    }
+
+
+
+
+
+}
