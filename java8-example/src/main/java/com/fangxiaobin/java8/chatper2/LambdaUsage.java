9e723e88e88f969b3d912980e6ba2e00f999eca4
forreche <516108074@qq.com>
2020-08-04 02:00:12 +0800

    Lambda 尝试

--- java8-example/src/main/java/com/fangxiaobin/java8/chatper2/LambdaUsage.java
+++ java8-example/src/main/java/com/fangxiaobin/java8/chatper2/LambdaUsage.java
@@ -1,15 +1,63 @@
 package com.fangxiaobin.java8.chatper2;
 
+import com.fangxiaobin.java8.chatper1.Apple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.LongPredicate;
+import java.util.function.Predicate;
+
 /**
  * @author :方晓彬
  * @Date :Created in 16:10 2020/7/31
- * description:
+ * description: lambda 用法
  */
 public class LambdaUsage {
 
+    private static List<Apple> filter(List<Apple> source, Predicate<Apple> predicate) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple a : source) {
+            if (predicate.test(a)) {
+                result.add(a);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * function 接口的使用
+     *
+     * @param source
+     * @param predicate
+     * @return
+     */
+    private static List<Apple> filterByWeight(List<Apple> source, LongPredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple a : source) {
+            if (predicate.test(a.getWeight())) {
+                result.add(a);
+            }
+        }
+
+        return result;
+    }
+
+
+    private static String testFunction(Apple apple, Function<Apple, String> function) {
+        return function.apply(apple);
+    }
+
+    private static Apple testBiFunction(String color, long weight, BiFunction<String, Long, Apple> fun) {
+        return fun.apply(color, weight);
+    }
+
 
     public static void main(String[] args) {
-        Runnable r1 = () -> System.out.println("Hello");
+       /* Runnable r1 = () -> System.out.println("Hello");
 
         Runnable r2 = new Runnable() {
             @Override
@@ -20,8 +68,24 @@ public class LambdaUsage {
 
         process(r1);
         process(r2);
-        process(() -> System.out.println("Hello"));
+        process(() -> System.out.println("Hello"));*/
 
+        List<Apple> list = Arrays.asList(
+                new Apple("green", 120),
+                new Apple("red", 150),
+                new Apple("green", 120),
+                new Apple("green", 120),
+                new Apple("green", 120)
+        );
+//        List<Apple> greenList = filter(list, (apple -> apple.getColor().equals("green")));
+//        System.out.println(greenList);
+//
+//        List<Apple> result = filterByWeight(list, w -> w > 120);
+//        System.out.println(result);
+
+
+        Apple a = testBiFunction("Blue", 130, (s, w) -> new Apple(s, w));
+        System.out.println(a);
 
 
     }
--- java8-example/src/main/java/com/fangxiaobin/java8/chatper3/SimpleStream.java
+++ java8-example/src/main/java/com/fangxiaobin/java8/chatper3/SimpleStream.java
@@ -9,24 +9,22 @@ import static java.util.stream.Collectors.toList;
  * @Date :Created in 17:39 2020/7/31
  * description: stream
  * 1.实例1 排序 挑选
- *
  */
 public class SimpleStream {
 
     public static void main(String[] args) {
 
         List<Dish> menu = Arrays.asList(
-                new Dish("pork",false,800,Dish.Type.MEAT),
-                new Dish("beef",false,700,Dish.Type.MEAT),
-                new Dish("chicken",false,400,Dish.Type.MEAT),
-                new Dish("french fries",true,530,Dish.Type.OTHER),
-                new Dish("rice",true,350,Dish.Type.OTHER),
-                new Dish("season fruit",true,120,Dish.Type.OTHER),
-                new Dish("pizza",true,550,Dish.Type.OTHER),
-                new Dish("prawns",false,300,Dish.Type.FISH),
-                new Dish("salmon",false,450,Dish.Type.FISH)
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
         );
-
 
 
         List<String> dishNamesByCollections = getDishNamesByCollections(menu);
@@ -37,12 +35,30 @@ public class SimpleStream {
         List<String> dishNamesByStreams = getDishNamesByStream(menu);
         System.out.println(dishNamesByStreams);
 
+
+        System.out.println("================================");
+
+        /**
+         * stream 的 使用方式
+         */
+        List<String> result = menu.stream().filter(d -> {
+            System.out.println("filtering->" + d.getName());
+            return d.getCalories() > 300;
+        }).map(d -> {
+            System.out.println("map -> " + d.getName());
+            return d.getName();
+        }).limit(3).collect(toList());
+
+
+        System.out.println(result);
+
+
     }
 
-    private static List<String> getDishNamesByStream(List<Dish> menu){
-       return menu.stream().filter(d -> d.getCalories() < 400 )
+    private static List<String> getDishNamesByStream(List<Dish> menu) {
+        return menu.stream().filter(d -> d.getCalories() < 400)
                 .sorted(Comparator.comparing(Dish::getCalories))
-                        .map(Dish::getName).collect(toList());
+                .map(Dish::getName).collect(toList());
     }
 
 
@@ -50,19 +66,21 @@ public class SimpleStream {
         List<Dish> lowCalories = new ArrayList<>();
 
         for (Dish d : menu) {
-            if(d.getCalories() < 400){
+            if (d.getCalories() < 400) {
                 lowCalories.add(d);
             }
         }
 
         // 排序
-        Collections.sort(lowCalories,(d1,d2) -> Integer.compare(d1.getCalories(),d2.getCalories()));
-        
+        Collections.sort(lowCalories, (d1, d2) -> Integer.compare(d1.getCalories(), d2.getCalories()));
+
         List<String> dishNameList = new ArrayList<>();
         for (Dish d : lowCalories) {
             dishNameList.add(d.getName());
         }
         return dishNameList;
+
+
     }
 
 }
